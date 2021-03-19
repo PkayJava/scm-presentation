4f7d4bcd0240bf64d716fc7e5367bc8e673ea904
Socheat KHAUV <khauv.socheat@jtrustsystem.co.jp>
2021-03-19 08:17:33 +0700

    add plus function

--- src/main/java/com/example/demo/BootApplication.java
+++ src/main/java/com/example/demo/BootApplication.java
@@ -3,7 +3,11 @@ package com.example.demo;
 public class BootApplication {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World " + plus(1, 2));
+    }
+
+    public static int plus(int a, int b) {
+        return a + b;
     }
 
 }
